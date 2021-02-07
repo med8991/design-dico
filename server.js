d530d17e3ce19342f4f852fc2808f27c91eaf627
Amine <aminbouali1995@hotmail.com>
2021-02-07 22:33:20 +0100

    hi

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 console.log(__dirname);
-res.sendFile(path.join(__dirname+'/../src/index.html'));
+res.sendFile(path.join(__dirname+'/../index.html'));
 });
 
 // Start the app by listening on the default Heroku port
