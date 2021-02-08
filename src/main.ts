741f484db9e4bb87f83c1eef9857ae9c954e25df
Mohamed Hassan <demahom1998@gmail.com>
2021-02-08 14:26:32 +0100

    config deployed version2

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,11 @@
 
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  */
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
 
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+
